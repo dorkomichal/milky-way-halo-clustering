d765466c7ee275af3af2ff1e6f4347f13ee743e7
Michal Dorko <md2018@cam.ac.uk>
2024-05-24 13:42:05 +0100

    Adding Tests

---
+++ tests/__init__.py
@@ -0,0 +1,5 @@
+import sys
+
+# As per https://stackoverflow.com/questions/54895002/modulenotfounderror-with-pytest
+# this is required for pytest to append current directory to $PYTHONPATH
+sys.path.append(".")
